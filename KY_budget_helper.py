b9653c22b87dee88b9a0516d986e9c1de20bc62f
IulianaIagodka <iuliana.iagodka@gmail.com>
2019-12-05 14:49:49 +0200

    removed secret

--- KY_budget_helper.py
+++ KY_budget_helper.py
@@ -1,17 +1,24 @@
-import json
+#import json
 import gspread
+
 from oauth2client.service_account import ServiceAccountCredentials
 
 # use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-client = gspread.authorize(creds)
 
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-sheet = client.open("pythontest").sheet1
+def main():
 
-# Extract and print all of the values
-list_of_hashes = sheet.get_all_records()
-print(list_of_hashes)
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
+
+    # Find a workbook by name and open the first sheet
+    # Make sure you use the right name here.
+    sheet = client.open("pythontest").sheet1
+
+    cell = sheet.cell(1, 2).value
+    sheet.update_cell(1, 2, "24.5")  # Update one cell
+    print(cell)
+
+if __name__ == '__main__':
+    main()
